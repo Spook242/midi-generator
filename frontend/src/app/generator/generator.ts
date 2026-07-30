afcc464602368c22c0b0c68b8d41d02150681db1
Spook242 <spook242@gmail.com>
2026-07-30 15:52:56 +0200

    feat(frontend): create generator UI and bind form inputs with ngModel

--- frontend/src/app/generator/generator.ts
+++ frontend/src/app/generator/generator.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-generator',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './generator.html',
-  styleUrl: './generator.css',
+  styleUrl: './generator.css'
 })
 export class Generator {
+  bpm: number = 120;
+  selectedKey: string = 'C';
+  selectedScale: string = 'MAJOR';
+
+  generateMidi() {
+    console.log('🎵 ¡Botón pulsado! Preparando datos para el servidor...');
+    console.log('Velocidad (BPM):', this.bpm);
+    console.log('Nota Raíz:', this.selectedKey);
+    console.log('Escala:', this.selectedScale);
+  }
+
 
 }
