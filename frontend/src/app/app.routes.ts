c1f1664901762c759d2a30918824e23110a61ed9
Spook242 <spook242@gmail.com>
2026-07-30 15:32:48 +0200

    feat(frontend): setup routing, home component and generator UI

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Generator } from './generator/generator';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'generator', component: Generator }
+];
