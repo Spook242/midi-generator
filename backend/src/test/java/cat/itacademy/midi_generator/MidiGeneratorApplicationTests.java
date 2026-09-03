1105915d00fec8318066d986f1d2b0a8a79d6cee
Spook242 <spook242@gmail.com>
2026-09-03 21:19:41 +0200

    test: configure h2 in-memory database for ci environment

--- backend/src/test/java/cat/itacademy/midi_generator/MidiGeneratorApplicationTests.java
+++ backend/src/test/java/cat/itacademy/midi_generator/MidiGeneratorApplicationTests.java
@@ -2,12 +2,13 @@ package cat.itacademy.midi_generator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MidiGeneratorApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
